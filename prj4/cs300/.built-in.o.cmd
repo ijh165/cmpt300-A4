fbe9f2944e6c1c83f40dec1751e677078b7f35b0
MontreaI <raymondchan1995@hotmail.com>
2016-04-05 15:06:24 -0700

    for loop and while loop is not done and also the assigning

--- prj4/cs300/.built-in.o.cmd
+++ prj4/cs300/.built-in.o.cmd
@@ -1,1 +1,1 @@
-cmd_cs300/built-in.o :=  ld -m elf_x86_64   -r -o cs300/built-in.o cs300/cs300_test.o cs300/array_stats.o 
+cmd_cs300/built-in.o :=  ld -m elf_x86_64   -r -o cs300/built-in.o cs300/cs300_test.o cs300/array_stats.o cs300/process_ancestors.o 
--- prj4/test-syscall/cs300_testapp.c
+++ prj4/test-syscall/cs300_testapp.c
@@ -3,11 +3,37 @@
 #include <unistd.h>
 #include <sys/syscall.h>
 
-#define _CS300_TEST_ 330
+#define _CS300_TEST_ 332
+#define ANCESTOR_NAME_LEN 16
+struct process_info {
+
+long pid;
+/* Process ID */
+
+char name[ANCESTOR_NAME_LEN];
+/* Program name of process */
+
+long state;
+/* Current process state */
+
+long uid;
+/* User ID of process owner */
+long nvcsw;
+/* # voluntary context switches */
+
+long nivcsw;
+/* # involuntary context switches */
+
+long num_children;
+/* # children process has */
+
+long num_siblings;
+/* # sibling process has */
+};
 
 int main(int argc, char *argv[]){
 	printf("\nDiving to kernel level\n\n");
-	int result = syscall(_CS300_TEST_, 12345);
+	int result = syscall(_CS300_TEST_, NULL, 5, NULL);
 	printf("\nRising to user level w/ result = %d\n\n", result);
 	
 	return 0;
