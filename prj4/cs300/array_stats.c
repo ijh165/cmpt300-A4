f34bbe97c7842d023422262fb086232c07dc5f25
MontreaI <raymondchan1995@hotmail.com>
2016-04-06 23:38:16 -0700

    lol

--- prj4/cs300/array_stats.c
+++ prj4/cs300/array_stats.c
@@ -4,48 +4,39 @@
 #include <asm/uaccess.h>
 //#include </usr/include/stdlib.h>
 
-struct array_statz{
-	long min;
-	long max;
-	long sum;
-};
+
 asmlinkage long sys_array_stats(struct array_stats *stats, long data[], long size){
-	struct array_statz structure;
-	long i = 0; 
-	long tmp = 0;
-	long tmp1 = 0;
-	long tmp2 = 0;
-	if(size < 0){
-		return -EINVAL;
-	}
-	else if(!stats){
-		return -EFAULT;
-	}
-	else{
-		if(copy_from_user(&data[0], &tmp, sizeof(long))){
+
+	struct array_stats statz;
+		long i = 0;	
+		long tmp = 0;
+
+	if(size <= 0) { return -EINVAL; } 
+	if(!stats) { return -EFAULT; } 
+
+		if(copy_from_user(&tmp, &data[0], sizeof(long))){
 			return -EFAULT;
 		}
-		structure.sum = structure.min = structure.max = tmp;
-			
-		for(i =1; i<size; i++){
-			if(copy_from_user(&data[i], &tmp, sizeof(long))){
+		else{
+			statz.sum = statz.min = statz.max = tmp;
+			printk(KERN_NOTICE "Sum=min=max = %li\n", tmp);
+		}
+	for(i =1; i<size; i++){
+		if(copy_from_user(&tmp, &data[i], sizeof(long))){
 				return -EFAULT;
 			}
-			if(tmp > structure.max){
-				structure.max = tmp;
-			}
-			else if(tmp < structure.min){
-				structure.min = tmp;
-			}
-			structure.sum += tmp;
+		if(tmp > statz.max){
+			statz.max = tmp;
 		}
-		
-		if(copy_to_user(&(structure.max), &tmp1, sizeof(long)) || copy_to_user(&(structure.min), &tmp2, sizeof(long)) || copy_to_user(&(structure.sum), &tmp2, sizeof(long))){
+		else if(tmp < statz.min){
+			statz.min = tmp;
+		}
+		statz.sum += tmp;
+	}
+
+	if(copy_to_user(&(stats->sum), &(statz.sum), sizeof(long)) || copy_to_user(&(stats->min), &(statz.min), sizeof(long)) || copy_to_user(&(stats->max), &(statz.max), sizeof(long))){
 		return -EFAULT;
-		}
-		stats->min = tmp;
-		stats->max = tmp1;
-		stats->sum = tmp2;
-		return 0;
-	} 		 
+	}
+	return 0;
+		 
 }
